e354e750fe74a814b60dd7eb126664aea073d48a
Allison Lacker <alacker@gmail.com>
2016-11-19 17:45:22 -0800

    Looks better on most graphs, but introduces wrong graphs with small x values

--- visualization.js
+++ visualization.js
@@ -13,7 +13,7 @@ var LABEL_SPACING = 40,
             return parseFloat(a, 10) * parseFloat(b, 10);
         },
         'div': function(a, b) {
-            if (b === 0) {
+            if (b <= 0.1 && b >= -0.1) {
                 return parseFloat(a, 10) / 0.1; // not accurate, but keeps the graph from exploding
             }
             return parseFloat(a, 10) / parseFloat(b, 10);
